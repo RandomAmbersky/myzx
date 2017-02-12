17a0a89c1411afbb70d6de08b304c7fdaddfff05
RandomAmbersky <random@random.ran>
2017-02-12 20:15:18 +0300

    no message

---
+++ main.asm
@@ -0,0 +1,15 @@
+DEVICE zxspectrum48
+org #6000;
+begin
+  ld hl,04000h
+  ld (hl),%10101010
+  ld de,04001h
+  ld bc,17FFh
+  ldir
+  di
+  halt
+  ret
+end
+
+display begin
+SAVESNA "myzx.sna",begin
