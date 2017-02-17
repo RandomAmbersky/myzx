a972b3a2cecf63c261826a8e74a0c5963f9cf5e4
RandomAmbersky <random@random.ran>
2017-02-18 02:47:01 +0300

    спрайты выводятся :)

--- lib/map.asm
+++ lib/map.asm
@@ -2,21 +2,53 @@
 ; Вход: DE - позиция в координатах карты
 ; A - номер спрайта
 map_call_show_sprite:
-  ADD D,D
-  ADD E,E
-  call screen_calc_screen_addr_DE
+
+  LD HL, DE ; DE x 2 - у нас ширина спрайта =2, то есть позиция 1x1 будет 2x2 в знакоместах
+  ADD HL, HL
+  LD DE, HL
+
+/*
+  LD L, A
+  LD H, 0; загружаем номер спрайта в HL
+  ADD HL,HL; x2
+  ADD HL,HL; x4
+  PUSH HL; // запоминаем x4
+  ADD HL,HL; x8
+  ADD HL,HL; x16
+  ADD HL,HL; x32
+  POP BC; // снимаем со стека x4 - еще 4 байта цветности
+  ADD HL, BC
+  LD BC, SPR_BEGIN;
+  ADD HL, BC;
+*/
+  LD HL, DE
+  call screen_calc_scr_addr_HL
+  LD DE, HL
   LD HL, test
-spr_loop_2:
-  LD A, #0F
+  LD A, #10
   PUSH DE
 spr_loop_1:
-    PUSH DE
-    LDI ; => LD (DE)(HL); INC DE; INC HL; DEC BC;
-    LDI
-    POP DE
-    EX AF, AF'
-    call screen_calc_down_DE
-    EX AF, AF'
-    DEC A
-    JR NZ,spr_loop_1
+  PUSH DE
+  LDI ; => LD (DE)(HL); INC DE; INC HL; DEC BC;
+  LDI
+  POP DE
+  EX AF, AF'
+  call screen_calc_down_DE
+  EX AF, AF'
+  DEC A
+  JR NZ,spr_loop_1
+
+  POP DE; // original screen position
+  call screen_addr_to_attr_DE
+  LDI
+  LDI
+  PUSH HL
+  LD BC, 30
+  LD HL, DE
+  ADD HL, BC
+  LD DE, HL
+  POP HL
+  LDI
+  LDI
+
   RET
--- lib/screen_utils.asm
+++ lib/screen_utils.asm
@@ -4,7 +4,11 @@
 ; программа вычисляет адрес экрана по знакоместам
 ; Вход: DE - позиция в знакоместах
 ; Выход: DE - экранный адрес
-screen_calc_screen_addr_DE:
+
+SCREEN_ADDR EQU #4000
+ATTR_ADDR EQU SCREEN_ADDR+#1800
+
+screen_calc_scr_addr_DE:
   LD A,E
   AND  7
   RRCA
@@ -17,6 +21,21 @@ screen_calc_screen_addr_DE:
   AND  #18
   OR   #40
   LD   D,A
+  RET
+
+screen_calc_scr_addr_HL:
+  LD A,L
+  AND 7
+  RRCA
+  RRCA
+  RRCA
+  ADD A,H
+  LD H,L
+  LD L,A
+  LD A,H
+  AND #18
+  OR #40
+  LD H,A
   RET
 
 ; перемещаемся на 1 знакоместо ниже
@@ -34,5 +53,19 @@ screen_calc_down_DE:
   RET C
   LD A,D
   SUB 8
+  LD D,A
+  RET
+
+; процедура пересчета адреса в экранной области
+; в адрес в области атрибутов
+; Вход: HL - адрес в экранной области
+; Выход: HL - адрес в области атрибутов
+screen_addr_to_attr_DE:
+  LD A,D
+  AND #18
+  RRCA
+  RRCA
+  RRCA
+  ADD A,ATTR_ADDR/#100
   LD D,A
   RET
