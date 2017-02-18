0c01e6251a60fdfdeba1771da36e42c46d0502ab
RandomAmbersky <random@random.ran>
2017-02-18 08:44:53 +0300

    чтобы взгляд мой стал суровей

--- main.asm
+++ main.asm
@@ -2,7 +2,9 @@ DEVICE zxspectrum48
 org #6000;
 prg_start
 
+  ;DI
   call map_show_map;
+  ;EI
 
 loop:
   JP loop
@@ -12,22 +14,22 @@ loop:
 
   ENDS
 
-include "ramka.asm"
+;include "ramka.asm"
 include "lib/screen_utils.asm"
 include "lib/map.asm"
 
 SPR_BEGIN:
 
   include "test.ASM"
-  include "wear_green.ASM"
+  ;include "wear_green.ASM"
 
 
-TEXT1  DEFB  22,3,12,16,7,17,2
-  DEFM  "TEMPORARY"
-TEXT1_end
-TEXT2  DEFB  22,5,12
-  DEFM  "CONSTANT"
-TEXT2_end
+;TEXT1  DEFB  22,3,12,16,7,17,2
+  ;DEFM  "TEMPORARY"
+;TEXT1_end
+;TEXT2  DEFB  22,5,12
+  ;DEFM  "CONSTANT"
+;TEXT2_end
 end_prg
 
   SAVESNA "myzx.sna",prg_start
