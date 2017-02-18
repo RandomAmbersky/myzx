ef519c8a7bae97deeeb4318804bf792f05312d9c
RandomAmbersky <random@random.ran>
2017-02-18 16:59:27 +0300

    убран лишний мусор файлов ( но они навеки останутся в ревизиях git )

--- main.asm
+++ main.asm
@@ -2,12 +2,11 @@ DEVICE zxspectrum48
 org #6000;
 prg_start
 
-  ;DI
   call map_show_map;
-  ;EI
-  ;LD HL, #0000
-  ;LD A, #00
-  ;call map_show_sprite
+
+  LD HL, #0F0B
+  LD A, #01
+  call map_show_sprite
 
 loop:
   JP loop
@@ -17,22 +16,12 @@ loop:
 
   ENDS
 
-;include "ramka.asm"
 include "lib/screen_utils.asm"
 include "lib/map.asm"
 
 SPR_BEGIN:
 
   include "test.ASM"
-  ;include "wear_green.ASM"
-
-
-;TEXT1  DEFB  22,3,12,16,7,17,2
-  ;DEFM  "TEMPORARY"
-;TEXT1_end
-;TEXT2  DEFB  22,5,12
-  ;DEFM  "CONSTANT"
-;TEXT2_end
 end_prg
 
   SAVESNA "myzx.sna",prg_start
