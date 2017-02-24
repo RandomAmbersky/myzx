8cc24aa9de3bf65941924aec8c0021c85e9fa083
RandomAmbersky <random@random.ran>
2017-02-24 19:34:56 +0300

    открыл для себя понятие модуля в ASM

---
+++ back/lib/define.asm
@@ -0,0 +1,10 @@
+
+STRUCT point
+posY byte 0
+posX byte 0
+ENDS
+
+STRUCT header
+width byte 0
+height byte 0
+ENDS
---
+++ engine/game.asm
@@ -0,0 +1,11 @@
+  MODULE game
+
+init  RET
+
+start
+  call input.get_sinclair_key
+  RET
+
+  ENDMODULE
+
+  include "engine/input.asm"
