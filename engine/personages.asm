2ec075d4ecd49598983eb011b638bfaf87afaa6e
RandomAmbersky <random@random.ran>
2017-02-27 15:04:19 +0300

    подвижки в сторону движения персонажа, но пока заготовки только

--- engine/personages.asm
+++ engine/personages.asm
@@ -19,6 +19,48 @@ name_p dw 0; pointer to name
 charge db 0; charge of nums of bullet in cell
 ENDS
 
+currPersonage dw 0; pointer to persArray
+
+; в поиске персонажа
+; Вход: DE - позиция на карте
+; Выход - A=1, в currPersonage указатель на персонажа
+;         A=0 если персонаж не найден
+find_at
+  XOR A
+  RET; не нашли :(
+
+; передвижение персонажа
+; указатель на текущего персонажа - IX
+move_up
+  LD A, (IX+pers.y)
+  DEC A
+  RET M
+  LD (IX+pers.y),A
+  RET
+
+move_down
+  LD A, (IX+pers.x)
+  DEC A
+  RET M
+  LD (IX+pers.x),A
+  RET
+
+move_right
+  LD A, (IX+pers.x)
+  INC A
+  CP mapSize
+  RET NC
+  LD (IX+pers.x),A
+  RET
+
+move_left
+  LD A, (IX+pers.x)
+  INC A
+  CP mapSize
+  RET NC
+  LD (IX+pers.x),A
+  RET
+
 init
   LD B, PersonagesNum
   LD HL, persArray
