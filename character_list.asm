257aac38921222240d5be26ea1ee0518f448bd23
RandomAmbersky <random@random.ran>
2017-03-04 00:09:38 +0300

    мелочь

--- character_list.asm
+++ character_list.asm
@@ -1,7 +1,7 @@
 
 ; y,x,action,action_max,hp,hp_max,strength,prot,sprite,floor,name_p
-HeroOne pers 5,5,20,20,100,100,10,50,#0a,0,HeroOneName
-Enemy pers 6,5,20,20,100,100,10,50,#0c,0,EnemyName
+HeroOne pers 5,5,20,20,100,100,10,50,10,0,HeroOneName
+Enemy pers 6,5,20,20,100,100,10,50,41,0,EnemyName
 PersonagesNum equ 2
 
 HeroOneName defb 'HeroOne', 0
