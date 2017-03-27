2f2d35c9d45de706a00cdbaf140c18a600f2a231
RandomAmbersky <random@random.ran>
2017-03-27 08:36:24 +0300

    текст ушел в папочку examples_text64, собирается и работает... все три заготовки собираются

--- magenuar/defines.asm
+++ magenuar/defines.asm
@@ -1,18 +1,12 @@
 
 ; MAP ZONE
 mapSize=32;100; размер карты; а в RebelStar2 - 50x80
-;DEFINE mapFile "dummy_map.asm"
-DEFINE mapFile "mage_map.asm"
-
-;  TILE ZONE REBELSTAR
-;DEFINE tileFile "rebelstar.asm"
-;DEFINE tileSize 2; сколько знакомест в одном спрайте
-;DEFINE show_tile screen.show_tile_2x2
+DEFINE mapFile "magenuar/mage_map.asm"
 
 ;  TILE ZONE MAGE
-DEFINE tileFile "mage_sprite.asm"
+DEFINE tileFile "magenuar/mage_sprite.asm"
 DEFINE tileSize 4; сколько знакомест в одном спрайте
 DEFINE show_tile screen.show_tile_4x4
 
 ; CHARACTERS ZONE
-DEFINE persFile "character_list.asm"
+DEFINE persFile "magenuar/character_list.asm"
--- murkn/defines.asm
+++ murkn/defines.asm
@@ -1,12 +1,12 @@
 
 ; MAP ZONE
 mapSize=32;100; размер карты; а в RebelStar2 - 50x80
-DEFINE mapFile "dummy_map.asm"
+DEFINE mapFile "murkn/dummy_map.asm"
 
 ;  TILE ZONE REBELSTAR
-DEFINE tileFile "rebelstar.asm"
+DEFINE tileFile "murkn/rebelstar.asm"
 DEFINE tileSize 2; сколько знакомест в одном спрайте
 DEFINE show_tile screen.show_tile_2x2
 
 ; CHARACTERS ZONE
-DEFINE persFile "character_list.asm"
+DEFINE persFile "murkn/character_list.asm"
