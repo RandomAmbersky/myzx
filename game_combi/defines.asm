293cd40482ae10adae5e29a7d96d246a8b7ca002
RandomAmbersky <random@random.ran>
2017-04-07 09:17:46 +0300

    no message

--- game_combi/defines.asm
+++ game_combi/defines.asm
@@ -10,3 +10,7 @@ mapSize=32;100; размер карты; а в RebelStar2 - 50x80
 ;  TILE ZONE
 DEFINE tileSize 2; сколько знакомест в одном спрайте
 DEFINE show_tile screen.show_tile_2x2; процедура показа спрайта
+
+; Our tileSize
+scrWidth equ 32/tileSize ; 32 знакоместа по горизонтали
+scrHeight equ 24/tileSize; 24 знакоместа по вертикали
